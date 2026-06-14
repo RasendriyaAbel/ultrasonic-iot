ff92f4396b5350900a39944499c69a214842abc9
Rasendriya Abel <rasendriyaabel68@gmail.com>
2026-06-14 15:31:33 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Jika deploy ke subfolder, ganti '/nama-subfolder/' dengan path publik aplikasi kamu.
+  // Contoh: base: '/ultrasonic-iot/'
+  base: '/ultrasonic-iot/',
   plugins: [react()],
   server: {
     proxy: {
